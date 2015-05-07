caea360a4534318994c84da131d3d1b920a62e2c
Sarah Bump <sbump90@gmail.com>
2015-05-07 15:07:28 -0500

    sarah's first push

--- Cups/app/controllers/aqua_controller.rb
+++ Cups/app/controllers/aqua_controller.rb
@@ -1,17 +1,46 @@
 class AquaController < ApplicationController
   def index
+    @aquas = Aqua.all
     #Bret does this
   end
 
   def show
+    @aqua = Aqua.find(params[:id])
     #Kishan does this
   end
 
   def new
+    @aqua = Aqua.new
     #Bret does this
+  end
+  def create
+    aqua = Aqua.create( aqua_params )
+    redirect_to "/aquas/#{ aqua.id }"
   end
 
   def edit
+    @aqua = Aqua.find(params[:id])
     #Sarah does this
   end
+  def update
+    aqua = Aqua.find(params[:id])
+    aqua.update( aqua_params )
+    redirect_to "/aquas/#{ aqua.id }"
+  end
+
+  private
+
+  def aqua_params
+    params.require(:aqua).permit(:name, :description)
+  endq
 end
+
+
+# aquas GET    /aquas(.:format)          aquas#index
+#       POST   /aquas(.:format)          aquas#create
+# new_aqua GET    /aquas/new(.:format)      aquas#new
+# edit_aqua GET    /aquas/:id/edit(.:format) aquas#edit
+#  aqua GET    /aquas/:id(.:format)      aquas#show
+#       PATCH  /aquas/:id(.:format)      aquas#update
+#       PUT    /aquas/:id(.:format)      aquas#update
+#       DELETE /aquas/:id(.:format)      aquas#destroy
--- Cups/config/routes.rb
+++ Cups/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'aqua/index'
-
-  get 'aqua/show'
-
-  get 'aqua/new'
-
-  get 'aqua/edit'
+resources :aquas 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
